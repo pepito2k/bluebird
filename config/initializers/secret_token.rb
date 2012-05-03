1849fdf23bef559f1d38efd0cd9e2ff280c007b5
Gonzalo Robaina <gonzalor@gmail.com>
2012-05-02 22:46:39 -0300

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluebird::Application.config.secret_token = '8c8bc2f5addcfd9302cccb84e9878622fbaba056a42b0dcbef6c9e2c8b0d7072cf45c0168fa8260795f9f7fbd4ee2136fb138a25df6c8f6ccee63cc307df446c'
