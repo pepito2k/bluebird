5fb7db1ccc3864133a6644b34c3ae7ca77cee2ce
Gonzalo Robaina <gonzalor@gmail.com>
2012-05-17 23:50:55 -0300

    Load layout by resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+
+  protected
+  def layout_by_resource
+    module_name = self.class.name.split("::").first
+    if devise_controller? || module_name == "Admin"
+      "admin"
+    else
+      "application"
+    end
+  end
+
 end
