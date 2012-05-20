38b410f44cee821a9da45e912a20ba3145776d28
Gonzalo Robaina <gonzalor@gmail.com>
2012-05-20 01:30:46 -0300

    Corrección en migración de categorías

--- db/migrate/20120517014427_create_categories.rb
+++ db/migrate/20120517014427_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string, :name
-      t.boolean, :active, :default => true
+      t.string :name
+      t.boolean :active, :default => true
       t.timestamps
     end
   end
