9edc56ed8b65ed73f43684b4ebeab3a941dcca99
Gonzalo Robaina <gonzalor@gmail.com>
2012-05-21 00:53:35 -0300

    ABM de categorías

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,40 @@
 class Admin::CategoriesController < ApplicationController
 
-  
+  def index
+    @categories = Category.all    
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to admin_categories_path, notice: "Category added successfully"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(params[:category])
+      redirect_to admin_categories_path, notice: "Category updated successfully"
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    if category.destroy
+      flash[:notice] = "Category deleted successfully"
+    else
+      flash[:notice] = category.errors.full_messages
+    end
+    redirect_to admin_categories_path
+  end
 
 end
