f1c83e31a5cf05b0bf5a13223c7372890587f34f
Gonzalo Robaina <gonzalor@gmail.com>
2012-06-02 21:39:45 -0300

    Interfaz de usuario para lectores

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    #raise Twitter.user('gonzalorobaina').inspect
-    @doc = Pismo::Document.new('http://www.rubyinside.com/cramp-asychronous-event-driven-ruby-web-app-framework-2928.html')
-    #raise @doc.inspect
+    @categories = Category.active    
+  end
 
-    #doc.title     # => "Cramp: Asychronous Event-Driven Ruby Web App Framework"
-    #doc.author    # => "Peter Cooper"
-    #doc.lede      # => "Cramp (GitHub repo) is a new, asynchronous evented Web app framework by Pratik Naik of 37signals (and the Rails core team). It's built around Ruby's EventMachine library and was designed to use event-driven I/O throughout - making it ideal for situations where you need to handle a large number of open connections (such as Comet systems or streaming APIs.)"
-    #doc.keywords  # => [["cramp", 7], ["controllers", 3], ["app", 3], ["basic", 2], ..., ... ]
-    
+  def read
+    @category = Category.find_by_slug(params[:category_slug])
+    articles = @category.articles.active.order("id desc").limit(15)
+    @pages = articles.each_slice(5).to_a
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :author, :body, :body_html, :url, :description, :date_published, :source_id
 
+  belongs_to :source
+
   validates_presence_of :title, :url
 
-  belongs_to :source
+  scope :active, where(:active => true)
+  scope :by_category, lambda{ |id| where(:category_id => id) }
 end
