709a8c44f6f355f4c2107acc9860ff662c3102cb
Gonzalo Robaina <gonzalor@gmail.com>
2012-06-11 22:34:16 -0300

    Convertí la indexación a espacios

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email
 
   def full_name
-  	"#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"
   end
 
   private
--- db/migrate/20120612011450_add_indexes_to_sources_categories_and_twits.rb
+++ db/migrate/20120612011450_add_indexes_to_sources_categories_and_twits.rb
@@ -1,7 +1,7 @@
 class AddIndexesToSourcesCategoriesAndTwits < ActiveRecord::Migration
   def change
-  	add_index :articles, :source_id
-  	add_index :sources, :category_id
-  	add_index :twits, :source_id
+    add_index :articles, :source_id
+    add_index :sources, :category_id
+    add_index :twits, :source_id
   end
 end
