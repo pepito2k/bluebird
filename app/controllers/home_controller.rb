29786597ac556135fde2091c13d4728f8178f646
Gonzalo Robaina <gonzalor@gmail.com>
2012-06-27 00:45:56 -0300

    Ordenar últimos artículos por peso

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,9 @@ class HomeController < ApplicationController
   def read
     @category = Category.find_by_slug(params[:category_slug])
     articles = @category.articles.active.order("id desc").limit(15)
-    @pages = articles.each_slice(5).to_a
+    articles_ids = articles.map { |i| i['id'] }
+    sorted_articles = @category.articles.where("articles.id in (#{articles_ids.join(',')})").active.order("weight desc")
+    @pages = sorted_articles.each_slice(5).to_a
   end
 
 end
